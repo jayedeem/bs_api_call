579f2d3788e6cb4760f52182d6ab69c2e0111a1b
root <root@ULTRA-CACAYURAN.ultra.local>
2020-03-24 11:59:43 -0700

    need to add logic

---
+++ config_api.py
@@ -0,0 +1,6 @@
+API = "4d3519876ee59a5fca4d90c2c0533103"
+URL = "https://api.broadsign.com:10889/rest/"
+HOST_ID = '/host/v17/by_id?'
+HEADERS = {'Authorization': 'Bearer ' + API}
+RES_QUERY = "resource_query/v3/query_by_name/"
+REQ_STATUS = 'monitor_poll/v2/by_client_resource_id?'
